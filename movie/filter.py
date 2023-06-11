cbc5af6baaab3bce7f548b4ad16fec19a0c486f1
Arshia izadyar <110552657+Arshia-Izadyar@users.noreply.github.com>
2023-06-11 16:44:55 +0330

    Delete movie directory

--- movie/filter.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-from movie.models import Movie
-
-class MovieFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains')
-    
-    class Meta:
-        model = Movie
-        fields = ('title', 'genre',)
