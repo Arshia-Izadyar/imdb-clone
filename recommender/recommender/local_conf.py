2886e774b00d13df30059089a4b58bb3d5d18aa9
Arshia izadyar <110552657+Arshia-Izadyar@users.noreply.github.com>
2023-06-14 09:22:24 -0700

    Add files via upload

---
+++ recommender/recommender/local_conf.py
@@ -0,0 +1,18 @@
+DATABASE = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'movies', 
+        'USER': 'arshia', 
+        'PASSWORD': 'a123',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
+    }
+}
+
+
+KEY = 'django-insecure-7a2d4)tf2fho8r+ogx8^w5*7fg0#3y@l^()r86d&4op=(5qs^%'
+EMAIL = 'arshiaa104@gmail.com'  
+HOST_PASS = "qvaldzcxbwmcazni" 
+GIT_HUB_CLIENT_ID = 'ba5edcb9d04d4b378019'
+GIT_HUB_SECRET = '3082d1a4b652e4204cd910314a83bb83acae208f'
+
